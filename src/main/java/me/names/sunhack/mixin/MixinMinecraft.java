f514ab5c10bc74b08b7513a946126ae086320d08
2b2t_names <muhammedhalibek@icloud.com>
2024-04-24 20:56:25 +0300

    1.0.0

--- src/main/java/me/names/sunhack/mixin/MixinMinecraft.java
+++ src/main/java/me/names/sunhack/mixin/MixinMinecraft.java
@@ -1,4 +1,18 @@
 package me.names.sunhack.mixin;
 
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
 public class MixinMinecraft {
+
+
+@Inject(method = "tick", at = @At("HEAD"))
+    public void tick(CallbackInfo ci) {
+    System.out.println("Client Working");
+}
 }
